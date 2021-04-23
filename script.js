7de856bb3166dcf2b99c9ff071686f2574432ec9
xandeq <xandeq@gmail.com>
2021-04-23 06:20:45 -0300

    Introdução ao JavaScript

---
+++ script.js
@@ -0,0 +1,10 @@
+var nome = "Alexandre Queiroz";
+var idade = 38;
+var idade50 = 50;
+var frase = "Vascão é o melhor time do mundo";
+
+//alert(idade + idade50);
+
+console.log(nome);
+console.log(idade+idade50);
+console.log(frase.replace("Vascão","Vasco").toLowerCase());
